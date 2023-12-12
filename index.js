3d5759df5a19bb257db46e2ce9a6e14509a897fe
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2023-12-12 22:33:18 +0500

    Homework 4.3dars

---
+++ index.js
@@ -0,0 +1,98 @@
+// Masalalar
+// 1.
+// let son1 = 3;
+// let son2 = 6;
+// let son3 = 10;
+
+// let juftVa5Kopaytma = son1 * son2 * son3;
+// console.log(juftVa5Kopaytma);
+
+// 1.  3 ta son berilgan. Berilgan sonlar orasidan juft hamda 5 ga karralilarini kopaytmasini topuvchi algoritm yozing
+// var a = 50;
+// var b = 25;
+// var c = 55;
+
+// var sum = 1;
+// if (a % 2 === 0 && a % 5 === 0) {
+//   sum *= a;
+// }
+// if (b % 2 === 0 && b % 5 === 0) {
+//   sum *= b;
+// }
+// if (c % 2 === 0 && c % 5 === 0) {
+//   sum *= c;
+// }
+// console.log(sum); // Natija: 10
+
+// 2. 4 ta son berilgan. Berilgan sonlar orasidan 3 ga yoki 7 ga karralilarining kopaytmasini topuvchi algoritmlar:
+
+// var a = 8;
+// var b = 5;
+// var c = 10;
+// var d = 3;
+
+// var sum = 1;
+// if (a % 3 === 0 || a % 7 === 0) {
+//   sum *= a;
+// }
+// if (b % 3 === 0 || b % 7 === 0) {
+//   sum *= b;
+// }
+// if (c % 3 === 0 || c % 7 === 0) {
+//   sum *= c;
+// }
+// if (d % 3 === 0 || d % 7 === 0) {
+//   sum *= d;
+// }
+
+// console.log(sum); // Natija: 126
+
+// 4. 2 xonali son berilgan. Berilgan sonning o'nlar xonasidagi son juft bo'lsa uning kvadratini // aks holda kubini topuvchi algoritm yozing
+
+// 5.  3 xonali son berilgan. Uning o'nlar hamda yuzlar xonasidagi sonlarni topuvchi algoritm yozing.
+
+// 6.  3 xonali son berilgan. Uning toq raqamlari raqamlari ko paytmasini topuvchi algoritm yozing
+
+// 7.  4 xonali son berilgan. Uning juft raqamlari sonini topuvchi algoritm yozing
+
+// 8.  // Sizga to'g'ri to'rtburchakning tomonlari a va b beriladi, siz uning yuzasi va
+// perametrini topib quyidagi shartga tekshirishingiz kerak bo'ladi. Agar yuzasi perimetridan katta bo'lsa  yuzasini aks holda peremetrini chiqaring.
+//
+
+// 9.  5 xonali son berilgan. Berilgan sondagi nollar sonini topuvchi algoritm yozing
+// let number = 50000;
+// let zero = 0;
+
+// while (number > 0) {
+//   if (number % 10 === 0) {
+//     zero++;
+//   }
+//   number = Math.floor(number / 10);
+// }
+
+// console.log(zero); // Natija: 4
+let number = 50000;
+let zero = 0;
+
+let  (number > 0) {
+  if (number % 10 === 0) {
+    zero++;
+  }
+  number = Math.floor(number / 10);
+}
+
+console.log(zero); // Natija: 4
+// 10. Olimning poyezdi soat t da ketishi kerak edi. Lekin u T soat kechikdi. Olimning poyezdi soat nechada ketishini  topuvchi algoritm yozing. Bu yerda (0 < t < 23)
+// let t = 22;
+// let tureTime = (t + 24) % 24;
+
+// console.log(tureTime);
+// var T = 18;
+// var t = 20;
+
+// var hoursToWait = t - T;
+// if (hoursToWait < 0) {
+//   hoursToWait = 24 + hoursToWait;
+// }
+
+// console.log(hoursToWait); // Natija: 2
